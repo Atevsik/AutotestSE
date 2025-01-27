582d2eea8bb337aaa6c8e25080e5d46eecc47405
Nick <nick.khudakov@gmail.com>
2025-01-27 19:19:18 +0400

    27.01.25

--- pages/pagefight.py
+++ pages/pagefight.py
@@ -9,7 +9,53 @@ class PageFight:
         self.browser = browser
 
     def open(self):
-        self.browser.get('https://www.sport-express.ru/tag/hoakin-bakli-25917/')
+        self.browser.get('https://www.sport-express.ru/live/mma/ufc/event-14640/')
+
+    def spisok_knopok(self):
+        spisok_knopok = self.browser.find_element(By.XPATH, "//div[@class='sp-matchcenter-event-page__buttons']")
+
+    def srav_stat(self):
+        srav_stat = self.browser.find_element(By.XPATH,"//div[@class='se-matchcenter-event-comparative-statistic-params']")
+
+    def last_fight(self):
+        last_fight = self.browser.find_element(By.XPATH,"//div[@class='sp-matchcenter-event-last-fight']")
+
+    def rulian(self):
+        rulian = self.browser.find_element(By.XPATH,"//p[contains(text(),'Раулиан')]")
+        rulian.click()
+        assert self.browser.current_url == 'https://www.sport-express.ru/tag/raulian-paiva-27821/', "Не правильный рулан"
+        self.browser.back()
+
+    def kori(self):
+        kori = self.browser.find_element(By.XPATH,"//p[contains(text(),'Кори')]")
+        kori.click()
+        assert self.browser.current_url == 'https://www.sport-express.ru/tag/kori-sendhagen-25243/'
+        self.browser.back()
+
+    def stat(self):
+        stat = self.browser.find_element(By.XPATH,"//span[@class='se-buttons-toggle-item-advanced__text sp-matchcenter-event-page__button-stats__text']")
+        stat.click()
+
+    def podbr_stat(self):
+        podbr_stat = self.browser.find_element(By.XPATH,"//div[@class='sp-matchcenter-event-stats__center']")
+
+    def bolshe_po_teme(self):
+        bolshe_po_teme = self.browser.find_element(By.XPATH,"//div[@class='se-news-topics-block']")
+
+    def vse_boi(self):
+        vse_boi = self.browser.find_element(By.XPATH,"//div[@class='sp-matchcenter-event-all-fights__header-title']")
+
+    def open_boi(self):
+        open_boi = self.browser.find_element(By.XPATH,"//div[@class='se-result-battle-score se-result-battle-score--size-sm se-result-battle-score--win-second']//div[@class='se-result-battle-score__result'][normalize-space()='KO']")
+        open_boi.click()
+        assert self.browser.current_url == 'https://www.sport-express.ru/live/mma/ufc/event-14699/', "Не правльный матч"
+
+    def legend(self):
+        legend = self.browser.find_element(By.XPATH,"//div[@class='sp-matchcenter-event-all-fights-mma__markings']")
+
+
+
+
 
 
 
