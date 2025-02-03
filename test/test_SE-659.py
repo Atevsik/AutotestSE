13a0f1d34cf6112d7d1fcf3c47fb4d1e7d142f7f
Nick <nick.khudakov@gmail.com>
2025-02-03 15:48:56 +0400

    3.02.25

---
+++ test/test_SE-659.py
@@ -0,0 +1,16 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from pages.basketeuro import BasketEuro
+
+
+def test_EURO_Basket(browser):
+    euro = BasketEuro(browser)
+    euro.open()
+    euro.menu_nadlogo()
+    euro.reklama()
+    euro.photo()
+    euro.podval()
+    euro.news()
+    euro.reviews()
+    euro.women()
+    euro.group(6)
