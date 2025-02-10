5440c94f1b5a2b6dd98ad925fd50bce584ba7967
Nick <nick.khudakov@gmail.com>
2025-02-10 17:38:01 +0400

    refactorV5

--- test/test_4966.py
+++ test/test_4966.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from pages.rplglavn import FootbalGlavn
 
 
-def test_rpl_gol(browser):
+def test_rpl_glavn(browser):
    glan_rpl = FootbalGlavn(browser)
    glan_rpl.open()
    glan_rpl.comands(16)
